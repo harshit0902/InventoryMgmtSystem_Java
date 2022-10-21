096c84c99029f5ca40813fa8b82af3bcf77e7302
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-10-21 19:23:42 +0530

    Update Cart.java

--- server/src/main/java/com/harshit/miniproject/model/Cart.java
+++ server/src/main/java/com/harshit/miniproject/model/Cart.java
@@ -1,5 +1,47 @@
 package com.harshit.miniproject.model;
 
-public class Cart {
+import java.util.ArrayList;
 
+public class Cart extends Item {
+    private int totalQty;
+    private double totalPrice;
+    private ArrayList<Item> it = new ArrayList<Item>();
+
+    public Cart() {
+        totalQty = 0;
+        totalPrice = 0.0;
+    }
+
+    public Cart(int totalQty, double totalPrice) {
+        this.totalQty = totalQty;
+        this.totalPrice = totalPrice;
+    }
+
+    public int getTotalQty() {
+        return totalQty;
+    }
+
+    public void setTotalQty(int totalQty) {
+        this.totalQty = totalQty;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public void addItem(Item i) {
+        it.add(i);
+    }
+
+    public void removeItem(Item i) {
+        it.remove(i);
+    }
+
+    public void changeQuantity(int count) {
+        it.quantity -= count;
+    }
 }
