d6806397d3ed2059b83e5b2f4bbb6a622065450d
Bujeetic-blink <87631619+Bujeetic-blink@users.noreply.github.com>
2022-10-22 15:50:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { createTheme } from '@mui/material/styles';
+
 import Login from './login'
+
 import Signup from './signup'
+
+// import Header from "./navbar1";
+// import Footer from "./footer";
+
 function App() {
-  return (
-    <div className="App">
-      <Signup/>
+   /* let theme = createTheme({
+        palette: {
+          primary: {
+            main: '#0052cc',
+          },
+          secondary: {
+            main: '#edf2ff',
+          },
+        },
+      });
       
-    </div>
-  );
+      theme = createTheme(theme, {
+        palette: {
+          info: {
+            main: theme.palette.secondary.main,
+          },
+        },
+      });
+*/
+    return (
+        <>
+        <div classname="page-container">
+        <div classname="content-wrap">
+        {/* <Navbar /> */}
+            <Routes>
+                <Route path='/' element={<App />}></Route>
+                
+                <Route path='/login' element={<Login />}></Route>
+                
+                <Route path='/signup' element={<Signup />}></Route>
+                
+                
+            </Routes>
+        </div>
+        </div>
+           
+        </>
+    )
 }
 
-export default App;
+export default App
