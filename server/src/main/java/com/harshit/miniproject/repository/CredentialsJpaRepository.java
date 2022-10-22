4e32839f7470a3ad8adf3e975439510126d8de67
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-10-22 22:01:37 +0530

    Update CredentialsJpaRepository.java

--- server/src/main/java/com/harshit/miniproject/repository/CredentialsJpaRepository.java
+++ server/src/main/java/com/harshit/miniproject/repository/CredentialsJpaRepository.java
@@ -2,12 +2,13 @@ package com.harshit.miniproject.repository;
 
 import com.harshit.miniproject.model.Credentials;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CredentialsJpaRepository extends JpaRepository<Credentials,String> {
+public interface CredentialsJpaRepository extends MongoRepository<Credentials,String> {
 
     //List<DiscussionIndex> findAllByDiscussionId(Long discussionID);
 }
