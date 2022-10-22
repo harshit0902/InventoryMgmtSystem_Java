578b64478bf007253901e28852c651b7d684a185
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-10-22 16:29:14 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './signup';
+import Login from './login'
+import Signup from './signup'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <>
+          <div id="add-background-img">
+              <BrowserRouter>
+                  <Routes>
+                      <Route path='/' element={<App />}></Route>
+                      <Route path='/login' element={<Login />}></Route>
+                      <Route path='/signup' element={<Signup />}></Route>
+                  </Routes>
+              </BrowserRouter>
+          </div>
+          </>
   </React.StrictMode>
 );
 
