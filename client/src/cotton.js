d10d3a6f5906cd65537a3fa0c059d09e4582f4db
Bujeetic-blink <87631619+Bujeetic-blink@users.noreply.github.com>
2022-10-28 00:37:18 +0530

    runtime error exists in cotton

--- client/src/cotton.js
+++ client/src/cotton.js
@@ -2,13 +2,46 @@ import { useState } from "react";
 
 
 
+
 function App() {
     
-    const [qty, setqty] = useState("");
-    const [quality1, setquality1] = useState("");
-    const [quality2, setquality2] = useState("");
-    const [quality3, setquality3] = useState("");
-
+    const [qty, setqty] = useState(0);
+    const [quality1, setquality1] = useState(0);
+    const [quality2, setquality2] = useState(0);
+    const [quality3, setquality3] = useState(0);
+    let q1=parseInt(quality1);
+    let q2=parseInt(quality2);
+    let q3=parseInt(quality3);
+    let qty1=parseInt(qty);
+    if(q1>9000&&q1<15000){
+      console.log("success");
+      
+    }  else{
+      console.log("failure");
+      alert("Input Invalid1"); 
+      }
+   if(q2>9000&&q2<15000){
+        console.log("success");
+        
+      }  else{
+        console.log("failure");
+        alert("Input Invalid2"); 
+     }  
+  if(q3>9000&&q3<15000){
+          console.log("success");
+          
+        }  else{
+          console.log("failure");
+          alert("Input Invalid3"); 
+    } 
+    if(qty1<=25){
+      console.log("success");
+      
+    }  else{
+      console.log("failure");
+      alert("Input Invalid4"); 
+      }
+    
 
 return (
     
@@ -26,11 +59,13 @@ return (
           onChange={(event) => { setqty(event.target.value); } }></input>
            <br></br>
       </div>
-      <label>Enter a value between 1-5. 5 being Highest quality while 1 being lowest quality</label>
+      <label>Enter a value between the range.</label>
       <div>
 
         <input type="number" placeholder='Quality 1'
           onChange={(event) => { setquality1(event.target.value); } }></input>
+          
+
            <br></br>
       </div>
       <div>
@@ -39,12 +74,15 @@ return (
           onChange={(event) => { setquality2(event.target.value); } }></input>
            <br></br>
       </div>
+      
       <div>
 
         <input type="number" placeholder='Quality 3'
           onChange={(event) => { setquality3(event.target.value); } }></input>
            <br></br>
       </div>
+      <button>Add to Cart</button>
+      <button>Special Request?</button>
 
     </div></>
 
