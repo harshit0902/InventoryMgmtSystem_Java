88f088cbfc54f3ac800f54ba7104dedfd438bb88
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-11-10 00:08:57 +0530

    Now append to object is working

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './specialrequestform';
+import App from './cart';
 import Login from './login'
 import Signup from './signup'
 import SpecialRequestForm from "./specialrequestform";
