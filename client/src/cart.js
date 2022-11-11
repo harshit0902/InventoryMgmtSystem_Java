58b0665068901cf2b5afe9aa33c06699c20e35c6
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-11-12 01:05:48 +0530

    Update cart.js

--- client/src/cart.js
+++ client/src/cart.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-testing 1 2 3
 
 let cart=[];
 //import { useState } from "react";
@@ -133,21 +132,56 @@ function App() {
         const bajobj =JSON.parse(baj);
         const rag= localStorage.getItem("ragData");
         const ragobj =JSON.parse(rag);
-        cart[0].itemName='A';
-        cart[1].itemName='B';
-        cart[2].itemName='C';
-        cart[3].itemName='D';
-        cart[4].itemName='E';
-        cart[5].itemName='F';
-        cart[6].itemName='G';
-        cart[7].itemName='H';
-        cart[8].itemName='I';
-        cart[9].itemName='J';
-        cart[0].qty=5;
-        cart[2].qty=15;
-        cart[4].qty=25;
-        cart[6].qty=35;
-        cart[8].qty=45;
+        cart[0].itemName='Cotton';
+        cart[1].itemName='Jute';
+        cart[2].itemName='Coffee';
+        cart[3].itemName='Steel';
+        cart[4].itemName='Aluminium';
+        cart[5].itemName='Copper';
+        cart[6].itemName='Wood';
+        cart[7].itemName='Wheat';
+        cart[8].itemName='Bajra';
+        cart[9].itemName='Ragi';
+        cart[0].qty=cottobj.quantity;
+        cart[1].qty=jutobj.quantity;
+        cart[2].qty=coffobj.quantity;
+        cart[3].qty=steelobj.quantity;
+        cart[4].qty=alumobj.quantity;
+        cart[5].qty=coppobj.quantity;
+        cart[6].qty=wooobj.quantity;
+        cart[7].qty=wheobj.quantity;
+        cart[8].qty=bajobj.quantity;
+        cart[9].qty=ragobj.quantity;
+        cart[0].q1=cottobj.quality_1;
+        cart[1].q1=jutobj.quality_1;
+        cart[2].q1=coffobj.quality_1;
+        cart[3].q1=steelobj.quality_1;
+        cart[4].q1=alumobj.quality_1;
+        cart[5].q1=coppobj.quality_1;
+        cart[6].q1=wooobj.quality_1;
+        cart[7].q1=wheobj.quality_1;
+        cart[8].q1=bajobj.quality_1;
+        cart[9].q1=ragobj.quality_1;
+        cart[0].q2=cottobj.quality_2;
+        cart[1].q2=jutobj.quality_2;
+        cart[2].q2=coffobj.quality_2;
+        cart[3].q2=steelobj.quality_2;
+        cart[4].q2=alumobj.quality_2;
+        cart[5].q2=coppobj.quality_2;
+        cart[6].q2=wooobj.quality_2;
+        cart[7].q2=wheobj.quality_2;
+        cart[8].q2=bajobj.quality_2;
+        cart[9].q2=ragobj.quality_2;
+        cart[0].q3=cottobj.quality_3;
+        cart[1].q3=jutobj.quality_3;
+        cart[2].q3=coffobj.quality_3;
+        cart[3].q3=steelobj.quality_3;
+        cart[4].q3=alumobj.quality_3;
+        cart[5].q3=coppobj.quality_3;
+        cart[6].q3=wooobj.quality_3;
+        cart[7].q3=wheobj.quality_3;
+        cart[8].q3=bajobj.quality_3;
+        cart[9].q3=ragobj.quality_3;
     }
 
     function display() {
