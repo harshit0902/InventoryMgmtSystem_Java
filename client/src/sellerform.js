99e76c39a7dca48c8e20b5d4b09ca20fd8453ec0
Bujeetic-blink <87631619+Bujeetic-blink@users.noreply.github.com>
2022-11-18 02:32:13 +0530

    seller form

---
+++ client/src/sellerform.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import axios from 'axios'
+
+
+
+function App()
+{
+   
+   
+   
+    return (
+    
+      
+        <><div>
+          <center>
+        <h1 id="Items_new_sell">
+           Cotton 
+           <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                Minimum quality required =" variable from backend"
+                Minimum quality required =" variable from backend"
+            <button onClick={()=> navigate('./sell_cotton')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Jute <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./jute')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Coffee<h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./coffee')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Steel <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./steel')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Aluminium <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./aluminium')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Copper <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./copper')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Wood <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./wood')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Wheat <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./wheat')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Bajra <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./bajra')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      <center>
+        <h1 id="Items_new_sell">
+           Ragi <h6>Quantity needed  ="variable from backend" 
+                Minimum quality required =" variable from backend" 
+                <button onClick={()=> navigate('./ragi')}>Supply now</button></h6>
+           
+        </h1>
+      </center>
+      </div></>
+       
+  
+  )
+
+}
