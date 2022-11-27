6251124e34dcf5327e43708db0325f56322b3b29
harshit0902 <83470228+harshit0902@users.noreply.github.com>
2022-11-27 12:49:11 +0530

    Update add_item.js

--- client/src/add_item.js
+++ client/src/add_item.js
@@ -1,16 +1,29 @@
 import { useState } from "react";
+import Axios from "axios";
 
 function App() {
     const [id, setid] = useState(0);
     const [name, setname] =useState("");
     const [qty, setqty] = useState(0);
+    const [price, setprice] = useState(0);
     const [quality1, setquality1] = useState(0);
     const [quality2, setquality2] = useState(0);
     const [quality3, setquality3] = useState(0);
-    
+
+    const savedetail = () => {
+        Axios.post('http://localhost:9091/api/admin/add', {
+            it: send
+        }).then((response) => {
+            // setUserdetails(response.data);
+            console.log(response)
+            if (response.data == 'success') {
+                //window.location.href = "http://localhost:3000/login";
+            }
+        });
+    }
+
 return (
-    
-      
+
     <><div>
   <center>
     <h1 id="cot">
@@ -18,12 +31,12 @@ return (
     </h1>
   </center>
 </div><div id="details">
-<div>
+        {/*<div>
       <label>Enter Item Id </label>
       <input type="number" placeholder='Item Id'
         onChange={(event) => { setid(event.target.value); } }></input>
          <br></br>
-    </div>
+    </div>*/}
 <div>
       <label>Enter Itemname </label>
       <input type="text" placeholder='Itemname'
@@ -31,17 +44,24 @@ return (
          <br></br>
     </div>
     <div>
-      <label>Enter Quantity required </label>
+      <label>Enter Quantity </label>
       <input type="number" placeholder='Enter in kg'
         onChange={(event) => { setqty(event.target.value); } }></input>
          <br></br>
     </div>
+
+        <div>
+            <label>Enter Price </label>
+            <input type="number" placeholder='Enter in Rs.'
+                   onChange={(event) => { setprice(event.target.value); } }></input>
+            <br></br>
+        </div>
     
     <div>
 
       <input type="number" placeholder='Quality 1'
        onChange={(event) => { setquality1(event.target.value); } } ></input>
-         
+
 
          <br></br>
     </div>
@@ -60,7 +80,7 @@ return (
     </div>
     
      <button onClick={() => {
-      
+         addItem()
            }}>Add Item</button>
     
 
