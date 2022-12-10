be53ec76f691ae369db8a49b050b1d90f7c09da8
r.navaneeth992 <r.navaneeth992@gmail.com>
2022-12-10 10:43:24 +0530

    Removed Comments

--- client/src/cart.js
+++ client/src/cart.js
@@ -6,143 +6,29 @@ import {Link} from "react-router-dom";
 
 let cart=[];
 let send=[];
-let s = "";
-let val=0;
-//import { useState } from "react";
 
 function Cart() {
-
-    //let [cart, setCart] = useState([]);
-    const cott= localStorage.getItem("cottData");
-    const cottobj =JSON.parse(cott);
-    const jut= localStorage.getItem("jutData");
-    const jutobj =JSON.parse(jut);
-    const coff= localStorage.getItem("coffData");
-    const coffobj =JSON.parse(coff);
-    const steel= localStorage.getItem("steelData");
-    const steelobj =JSON.parse(steel);
-    const alum= localStorage.getItem("alumData");
-    const alumobj =JSON.parse(alum);
-    const copp= localStorage.getItem("coppData");
-    const coppobj =JSON.parse(copp);
-    const woo= localStorage.getItem("wooData");
-    const wooobj =JSON.parse(woo);
-    const whe= localStorage.getItem("wheData");
-    const wheobj =JSON.parse(whe);
-    const baj= localStorage.getItem("bajData");
-    const bajobj =JSON.parse(baj);
-    const rag= localStorage.getItem("ragData");
-    const ragobj =JSON.parse(rag);
-    /*const [quantity, setquantity] = useState(0);
-    const [quality1, setquality1] = useState(0);
-    const [quality2, setquality2] = useState(0);
-    const [quality3, setquality3] = useState(0);
-    let quality1=parseInt(quality1);
-    let quality2=parseInt(quality2);
-    let quality3=parseInt(quality3);
-    let quantity1=parseInt(quantity);*/
-    //const cart = [];
-    // const [cart,setCart]=useState([]);
-    //setCart([...textarr,{parsedobj }]);
-    // function localstorage1() {
-
-    // console.log(parsedobj);
-    // }
-// function val() {
-//     const dtr= localStorage.getItem("steelData");
-//     console.log(dtr);
-//     const parsedobj =JSON.parse(dtr);
-//     let i = 'steel';
-//     // setCart([...cart, {itemName:i, quantity:0, quality1:0, quality2:0, quality3:0}]);
-//     const cart1 = {
-//      itemNAme : "steel",
-//       q_1 : parsedobj.quality_1 ,
-//      q_2 : parsedobj.quality_2 ,
-
-//           itemNAme : "Cotton"
-//     }
-//     // console.log(cart);
-//     // cart[0].itemName = 'Steel';
-//     // cart[0].quantity = parsedobj.quantity;
-//     // console.log(cart1[0].itemName);
-//     // console.log(cart1[0].quantity);
-//     console.log(cart1);
-//     const jsonobj1 = JSON.stringify(cart1);
-//     console.log(jsonobj1);
-//     localStorage.setItem("cart1",jsonobj1);
-
-//     const ctr= localStorage.getItem("cart1");
-
-    /*function val() {
-        const dtr= localStorage.getItem("steelData");
-        console.log(dtr);
-        const parsedobj =JSON.parse(dtr);
-        let i = 'steel';
-        // setCart([...cart, {itemName:i, quantity:0, quality1:0, quality2:0, quality3:0}]);
-        const cart1 = {
-         itemNAme : "steel",
-          q_1 : parsedobj.quality_1 ,
-         q_2 : parsedobj.quality_2 ,
-
-              itemNAme : "Cotton"
-        }*/
-    // console.log(cart);
-    // cart[0].itemName = 'Steel';
-    // cart[0].quantity = parsedobj.quantity;
-    // console.log(cart1[0].itemName);
-    // console.log(cart1[0].quantity);
-    /*console.log(cart1);
-    const jsonobj1 = JSON.stringify(cart1);
-    console.log(jsonobj1);
-    localStorage.setItem("cart1",jsonobj1);
-
-    const ctr= localStorage.getItem("cart1");
-    const parsedobj1 =JSON.parse(ctr);
-    console.log(parsedobj1);
-    console.log(parsedobj1.q_1,parsedobj1.q_2,parsedobj1.itemNAme[0],parsedobj1.itemNAme[1]);
-}*/
-//     const parsedobj1 =JSON.parse(ctr);
-//     console.log(parsedobj1);
-//     console.log(parsedobj1.q_1,parsedobj1.q_2,parsedobj1.itemNAme[0],parsedobj1.itemNAme[1]);
-// }
-// let [cart,setCart]=useState([]);
-    /*function val() {
-        let i = 'steel';
-        // setCart([...cart, {itemName:i, quantity:0, quality1:0, quality2:0, quality3:0}]);
-        cart.push({itemName:i, quantity:0, quality1:0, quality2:0, quality3:0});
-        // console.log(cart);
-        cart[0].itemName='Wheat';
-        console.log(cart);
-        /*const ctr= localStorage.getItem("bajData");
-        const parsedobj1 =JSON.parse(ctr);
-        cart[0].itemName = 'baj';
-        cart[0].quantity = parsedobj1.quantity;
-        cart[0].quality1 = parsedobj1.quality_1;
-        cart[0].quality2 = parsedobj1.quality_2;
-        cart[0].quality3 = parsedobj1.quality_3;
-        console.log(cart);*/
-    //}
-    // const deleteval = () {
-    //   return(
-    //     <div>
-    //     {cart.map((item)=>(
-    //         item.quantity>0 ? (
-    //             <div key={item.itemName}>
-    //                 <h5> Item Name: {item.itemName} </h5>
-    //                 <h5> Quantity: {item.quantity} </h5>
-    //                 <h5> Quality 1: {item.quality1} </h5>
-    //                 <h5> Quality 2: {item.quality2} </h5>
-    //                 <h5> Quality 3: {item.quality3} </h5>
-    //                 <h5><button onClick={ ()=> {item.quantity=0,item.quality1=0,item.quality2=0,item.quality3=0
-    //                ,deleteval }
-    //                }>Delete</button></h5>
-    //                 -------------------------------------
-    //             </div>
-    //         ) : ("") )
-    //     )}
-    // </div>
-    //   )
-    // }
+    let cott= localStorage.getItem("cottData");
+    let cottobj =JSON.parse(cott);
+    let jut= localStorage.getItem("jutData");
+    let jutobj =JSON.parse(jut);
+    let coff= localStorage.getItem("coffData");
+    let coffobj =JSON.parse(coff);
+    let steel= localStorage.getItem("steelData");
+    let steelobj =JSON.parse(steel);
+    let alum= localStorage.getItem("alumData");
+    let alumobj =JSON.parse(alum);
+    let copp= localStorage.getItem("coppData");
+    let coppobj =JSON.parse(copp);
+    let woo= localStorage.getItem("wooData");
+    let wooobj =JSON.parse(woo);
+    let whe= localStorage.getItem("wheData");
+    let wheobj =JSON.parse(whe);
+    let baj= localStorage.getItem("bajData");
+    let bajobj =JSON.parse(baj);
+    let rag= localStorage.getItem("ragData");
+    let ragobj =JSON.parse(rag);
+    
     const savedetail = () => {
         for(let i=0 ; i<cart.length ; i++)
         {
@@ -150,20 +36,16 @@ function Cart() {
                 send.push(cart[i]);
             }
         }
-        const pass = {"it":send}
         console.log(send);
-        //console.log(pass);
         Axios.post('http://localhost:9091/api/buy/normal', {
             it: send
         }).then((response) => {
-            // setUserdetails(response.data);
             console.log(response)
             if (response.data == 'success') {
                 window.location.href = "http://localhost:3000/login";
             }
         });
     }
-    //function multiple() {
     let i1 = 'a';
     let i2 = 'b';
     let i3 = 'c';
@@ -184,7 +66,6 @@ function Cart() {
     cart.push({itemName:i8, quantity:0, quality1:0, quality2:0, quality3:0});
     cart.push({itemName:i9, quantity:0, quality1:0, quality2:0, quality3:0});
     cart.push({itemName:i10, quantity:0, quality1:0, quality2:0, quality3:0});
-    //console.log(cart);
     cart[0].itemName='Cotton';
     cart[1].itemName='Jute';
     cart[2].itemName='Coffee';
@@ -235,46 +116,6 @@ function Cart() {
     cart[7].quality3=wheobj.quality_3;
     cart[8].quality3=bajobj.quality_3;
     cart[9].quality3=ragobj.quality_3;
-    /*for(let i=0 ; i<cart.length ; i++)
-    {
-        if(cart[i].quantity > 0){
-          console.log(cart[i].quantity+1);
-            console.log(cart[i].itemName);}
-    }*/
-    //}
-    /*function display() {
-        /*return (
-            <>
-                <div>
-                    {cart.map((item)=>(
-                            <div key={item.itemName}>
-                                <h5> Item Name: {item.itemName} </h5>
-                                <h5> Quantity: {item.quantity} </h5>
-                                <h5> Quality 1: {item.quality1} </h5>
-                                <h5> Quality 2: {item.quality2} </h5>
-                                <h5> Quality 3: {item.quality3} </h5>
-                                -------------------------------------
-                            </div>
-                        )
-                    )}
-                </div>
-            </>)*/
-    /*useEffect(() => {
-        cart.map((item)=>(
-            item.quantity>0 ? (
-                <div key={item.itemName}>
-                    <h5> Item Name: {item.itemName} </h5>
-                    <h5> Quantity: {item.quantity} </h5>
-                    <h5> Quality 1: {item.quality1} </h5>
-                    <h5> Quality 2: {item.quality2} </h5>
-                    <h5> Quality 3: {item.quality3} </h5>
-                    <h5><button onClick={ ()=>{console.log(item); item.quantity=0; item.quality1=0; item.quality2=0; item.quality3=0; display()}}>Delete</button></h5>
-                    -------------------------------------
-                </div>
-            ) : ("") )
-        )
-    });*/
-
     function display(n)
     {
         if(n === "Cotton")
@@ -367,25 +208,15 @@ function Cart() {
             console.log(n);
             return true;
         }
-        /*cart.map((item)=>(
-            item.quantity>0 ? (
-                <div key={item.itemName}>
-                    <h5> Item Name: {item.itemName} </h5>
-                    <h5> Quantity: {item.quantity} </h5>
-                    <h5> Quality 1: {item.quality1} </h5>
-                    <h5> Quality 2: {item.quality2} </h5>
-                    <h5> Quality 3: {item.quality3} </h5>
-                    <h5><button onClick={ ()=>{console.log(item); display(item)}}>Delete</button></h5>
-                    -------------------------------------
-                </div>
-            ) : ("") )
-        )*/
         return false;
     }
     function update(it)
     {
         console.log(it);
-        cart = cart.filter(display);
+        cart[it].quantity=0;
+        cart[it].quality1=0;
+        cart[it].quality2=0;
+        cart[it].quality3=0;
     }
     return (
         <><Header /><div>
@@ -395,16 +226,9 @@ function Cart() {
                 </h1>
             </center>
         </div>
-            {/*for(i=0 ; i<cart.length ; i++)
-        {
-            if(cart[i].quantity > 0){
-            s = s + "Item Name: " + cart[i].itemName + ", Quantity: " + cart[i].quantity + ", Quality 1: " + cart[i].quality1 + ", Quality 2: " + cart[i].quality2 + "Quality 3: " + cart[i].quality3;
-            {s}
-            }
-            s = "";
-        }*/}
             <div>
-                {cart.map((item)=>(
+                {cart.map((item,val)=>(
+                    val+1,
                     item.quantity>0 ? (
                         <div key={item.itemName}>
                             <h5> Item Name: {item.itemName} </h5>
@@ -412,34 +236,13 @@ function Cart() {
                             <h5> Quality 1: {item.quality1} </h5>
                             <h5> Quality 2: {item.quality2} </h5>
                             <h5> Quality 3: {item.quality3} </h5>
-                            {/*let obj = {
-                            names: item.itemName,
-                            quantity: item.quantity,
-                            quality1: item.quality1,
-                            quality2: item.quality2,
-                            quality3: item.quality3
-                        }*/}
-                            {/*<h5><button onClick={update(item)}>Delete</button></h5>*/}
+                            <h5><button onClick={()=>{}}>Delete</button></h5>
                             -------------------------------------
                         </div>
                     ) : ("") )
                 )}
             </div>
             <button onClick={savedetail}><Link id="sign" to='/homepage2'>Confirm Cart</Link></button>
-            {/*<button onClick={val}>Hello</button>
-      {
-        cart.map((val)=>{
-          return <>
-            <div>
-              {val.itemName}
-              <br></br>
-              {val.quality1}
-            </div>
-          </>
-        })
-       }*/}
-            {/* cart[0].itemName = "Steel"; */}
-            {/* <button>Special Request?</button> */}
 
         <Footer /></>
     )
